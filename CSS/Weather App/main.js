306fa0daa44d26aeb0d7f8e8f8c2f2440e65205a
Aniket Saha <anikets349@gmail.com>
2023-02-21 16:09:45 +0530

    feat: Add hourly forecast section

--- CSS/Weather App/main.js
+++ CSS/Weather App/main.js
@@ -2,28 +2,58 @@ const API_KEY = 'e6a7b9d25322776f6cef20e87dfcba86';
 
 const getCurrentWeatherData = async function () {
     const city = 'belagavi';
-    const response = await fetch(`https://apii.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     return response.json();
 };
 
-const formatTemperature = function (temp) {
-    return `<span>${temp.toFixed(1)}&deg;C</span>`;
+const getHourlyForecast = async function ({ name: city }) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    return data.list.map(obj => {
+        const { dt, dt_txt, main: { temp, temp_max, temp_min }, weather: [{ description, icon }] } = obj;
+        return { dt, dt_txt, temp, temp_max, temp_min, description, icon };
+    });
 };
+
+const formatTemperature = function (temp) {
+    return `<span>${temp?.toFixed(1)}&deg;C</span>`;
+};
+
+const createImgUrl = function (icon) {
+    return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+}
 
 const loadCurrentWeatherInfo = function ({ name, main: { temp, temp_max, temp_min }, weather: [{ description }] }) {
     const currentForecastElement = document.getElementById('current-forecast');
-    console.log(currentForecastElement);
     currentForecastElement.querySelector('.city').textContent = name;
     currentForecastElement.querySelector('.temp').innerHTML = formatTemperature(temp);
     currentForecastElement.querySelector('.desc').textContent = description;
     currentForecastElement.querySelector('.min-max-temp').innerHTML = `High: ${formatTemperature(temp_max)}, Low: ${formatTemperature(temp_min)}`;
 };
 
+const loadHourlyForecast = function (hourlyForecast) {
+    let hourlyData = hourlyForecast.slice(0, 13);
+    console.log(hourlyData);
+    const hourlyContainer = document.getElementById('hourly-container');
+    let data = ``;
+    for (let { dt_txt: time, icon, temp } of hourlyData) {
+        data += `
+            <article>
+                    <h3 class="time">${time.split(' ')[1]}</h3>
+                    <img class="icon" src="${createImgUrl(icon)}" alt="Current Weather icon">
+                    <p class="temp">${formatTemperature(temp)}</p>
+                </article>
+        `;
+        hourlyContainer.innerHTML = data;
+    }
+};
+
 document.addEventListener('DOMContentLoaded', async function () {
     try {
         const currentWeather = await getCurrentWeatherData();
-        console.dir(currentWeather);
         loadCurrentWeatherInfo(currentWeather);
+        const hourlyForecast = await getHourlyForecast(currentWeather);
+        loadHourlyForecast(hourlyForecast);
     } catch (err) {
         console.log('Error: ' + err.message);
     }
